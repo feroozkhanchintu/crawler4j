1539f921403c0057afc27a9ad36f3555c12cbc00
feroozkhanchintu <feroozkhan.chintu@gmail.com>
2016-09-09 17:09:19 +0600

    File Refactored : src/main/java/edu/uci/ics/crawler4j/url/UrlResolver.java

--- src/main/java/edu/uci/ics/crawler4j/url/UrlResolver.java
+++ src/main/java/edu/uci/ics/crawler4j/url/UrlResolver.java
@@ -12,7 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. ABCCD192839812323123321123123
+ * limitations under the License. ABCCD192839812323123321123123233
  */
 
 package edu.uci.ics.crawler4j.url;
